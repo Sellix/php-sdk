42f2583ae7cffad5e375ae877cf7d2e70fb3eedf
Daniele Servadei <servadei.d@gmail.com>
2022-01-02 14:27:03 +0100

    remove namespace from Exception class

--- src/Exceptions/index.php
+++ src/Exceptions/index.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Sellix\PhpSdk;
-
 class SellixException extends Exception {
   public function __construct($message, $code = 0, Throwable $previous = null) {
     parent::__construct($message, $code, $previous);
