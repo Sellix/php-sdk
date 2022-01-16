ee8203f08560fb5531a3b93bedac16f2811b4bbc
Daniele Servadei <servadei.d@gmail.com>
2022-01-16 16:16:02 +0100

    update example.php

--- example.php
+++ example.php
@@ -4,18 +4,18 @@ require_once "src/Sellix.php";
 
 // pass <MERCHANT_NAME> only if you need to be authenticated as an additional store
 
-$sellix = new \Sellix\PhpSdk\Sellix("HuKWSxeQiT04tsrFAf9jHdjZCEiA3UI6sKEctGcQMM3aDIO1Vsmrk7XI8THftklt", "Daniele");
+$sellix = new \Sellix\PhpSdk\Sellix("<MERCHANT_API_KEY>", "<MERCHANT_NAME>");
 
 sellix_test_sdk($sellix, [
-  // "blacklists",
-  // "whitelists",
-  // "categories",
-  // "coupons",
-  // "feedback",
-  // "orders",
-  // "products",
-  // "queries",
-  // "payments",
+  "blacklists",
+  "whitelists",
+  "categories",
+  "coupons",
+  "feedback",
+  "orders",
+  "products",
+  "queries",
+  "payments",
   "subscriptions",
-  // "customers"
+  "customers"
 ]);
